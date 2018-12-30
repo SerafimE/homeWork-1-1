5d42de80e0f59ef090d00429cce5d37703e7a90d
SerafimE <38590113+SerafimE@users.noreply.github.com>
2018-12-30 13:35:20 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,30 +1,32 @@
 <?php declare(strict_types=1);
 
 $name = 'Сергей';
+$surname = 'Люляев';
 $age = 41;
 $email = 'c_76@bk.ru';
 $city = 'Санкт-Петербург';
 $about = 'Начинающий Веб-разработчик';
 
-function number($x)
+function fibonacci($x)
 {
     $first = $second = 1;
     do {
         $third = $first;
         $first += $second;
         $second = $third;
-        if (($first == $x) || ($x == 1)) {
+        if ($first == $x || $x == 1) {
             return true;
         } elseif ($first > $x) {
             return false;
         }
     } while ($first < $x);
+    return $x;
 }
 
-if (isset($_GET['number'])) {
-    $num = $_GET['number'];
-    $inNumberRow = "Задуманное число " . $num . " входит в числовой ряд";
-    $notInNumberRow = "Задуманное число " . $num . " НЕ входит в числовой ряд";
+if (isset($_POST['number'])) {
+    $x = $_POST['number'];
+    $yes = "Ведённое число $x входит в числовой ряд!";
+    $no = "Ведённое число $x НЕ входит в числовой ряд!";
 }
 ?>
 <!DOCTYPE html>
@@ -35,40 +37,43 @@ if (isset($_GET['number'])) {
     <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
-<div>
-    <h1>Страница пользователя Сергей Люляев</h1>
-    <table>
-        <tr>
-            <td>Имя</td>
-            <td><?php echo $name ?></td>
-        </tr>
-        <tr>
-            <td>Возраст</td>
-            <td><?php echo $age ?></td>
-        </tr>
-        <tr>
-            <td>Адрес электронной почты</td>
-            <td><a href="mailto:<?php echo $email ?>"><?php echo $email ?></a></td>
-        </tr>
-        <tr>
-            <td>Город</td>
-            <td><?php echo $city ?></td>
-        </tr>
-        <tr>
-            <td>О себе</td>
-            <td><?php echo $about ?></td>
-        </tr>
-    </table>
-</div>
-<hr>
-<a href="https://goo.gl/8SdXcu" target="_blank"><h1>Числа Фибоначчи в Wikipedia</h1></a>
-<hr>
-<form action="index.php">
+<h1>Страница пользователя <?php echo "$name $surname" ?></h1>
+<img src="https://pp.userapi.com/c840239/v840239323/6c98e/HNG47eps4Xc.jpg" alt="<?php echo "$name $surname" ?>">
+<table>
+    <tr>
+        <td>Имя</td>
+        <td><?php echo $name ?></td>
+    </tr>
+    <tr>
+        <td>Фамилия</td>
+        <td><?php echo $surname ?></td>
+    </tr>
+    <tr>
+        <td>Возраст</td>
+        <td><?php echo $age ?></td>
+    </tr>
+    <tr>
+        <td>Адрес электронной почты</td>
+        <td><a href="mailto:<?php echo $email ?>"><?php echo $email ?></a></td>
+    </tr>
+    <tr>
+        <td>Город</td>
+        <td><?php echo $city ?></td>
+    </tr>
+    <tr>
+        <td>О себе</td>
+        <td><?php echo $about ?></td>
+    </tr>
+</table>
+<a href="https://goo.gl/8SdXcu" target="_blank"><h2>Числа Фибоначчи на wikiwand</h2></a>
+<form action="dz_1_2.php" method="post">
     <input type="number" placeholder="Введите число" name="number">
     <button type="submit">Проверить</button>
 </form>
-<?php if (isset($num)) : ?>
-    <p><?php echo(number($num) ? $inNumberRow : $notInNumberRow); ?></p>
-<?php endif;?>
-    </body>
+<p>
+    <?php if (isset($x)) {
+        echo(fibonacci($x) ? $yes : $no);
+    } ?>
+</p>
+</body>
 </html>
